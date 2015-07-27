41293124c278ac13808572ab61faf14fe2e9c9dc
Cosmitar <mhherrera31@gmail.com>
2015-07-27 02:19:16 -0300

    add /public as static files folder

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 module.exports = function() {
-    var app = require('express')();
+    var express = require('express');
+    var app = express();
     var http = require('http').Server(app);
     var io = require('socket.io')(http);
     var routes = require('./routes');
@@ -9,6 +10,7 @@ module.exports = function() {
     app.use('/', routes);
     app.set('views', __dirname + '/views');
     app.engine('html', require('ejs').renderFile);
+    app.use(express.static('public'));
 
     http.listen(3000, function() {
         console.log('listen');
