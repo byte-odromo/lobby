4b22f522e21e19832acd3bfd963c3979e1e9a4ff
Joel <joelalovera@gmail.com>
2015-08-09 19:38:09 -0300

    integrate joystick and clean code

--- app/controllers/indexController.js
+++ app/controllers/indexController.js
@@ -1,10 +1,3 @@
-var utils = require('../utils');
-
-// indexController
-/*var indexController = function(req, res, next) {
-	var hash = utils.generateHash();
-	res.redirect('/game/'+hash);
-};*/
 var indexController = function(req, res, next) {
 	res.render('index.html');
 };
--- app/routes.js
+++ app/routes.js
@@ -2,10 +2,5 @@ var express = require('express'),
 	router = express.Router();
 
 router.get('/', require('./controllers/indexController'));
-router.get('/test', function( req, res, next ){
-	res.render('test.html');
-});
-
-router.get('/game/:id', require('./controllers/gameController'));
 
 module.exports = router;
